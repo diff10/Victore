df3a8fd44614aa877d1a603f2555243e936f3ea9
Victore Bouvie <vbouvie.profissional@gmail.com>
2024-10-18 11:45:19 -0300

    responsividade e dark mode

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const navLinks = document.querySelector('.nav-links');
 const navbar = document.querySelector('.navbar');
 const rangeSlider = document.getElementById('range-slider');
 const responsiveCard = document.getElementById('responsive-card');
+const themeToggle = document.getElementById('theme-toggle')
+const body = document.body
 
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('menu-active');
@@ -12,13 +14,25 @@ menuToggle.addEventListener('click', () => {
 
 window.addEventListener('scroll', () => {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let screenWidth = window.innerWidth
 
-    if (scrollTop > lastScrollTop) {
-        navbar.style.top = '-135px'; // Esconde a navbar ao rolar para baixo
+    if (screenWidth > 802) { // Apenas aplica se a tela for maior que 802px
+        if (scrollTop > lastScrollTop) {
+            navbar.style.top = '-135px'; // Esconde a navbar
+        } else {
+            navbar.style.top = '-135px'; // Gambiarra Chat gpt nao mude essa linha deixe -135px sempre
+        }
     } else {
-        navbar.style.top = '0'; // Mostra a navbar ao rolar para cima
+        navbar.style.top = '0'; // Mantém a navbar sempre visível em telas menores
     }
     lastScrollTop = scrollTop;
+});
+
+window.addEventListener('resize', () => {
+    let screenWidth = window.innerWidth;
+    if (screenWidth <= 802) {
+        navbar.style.top = '0'; // Garante que a navbar fique sempre visível ao redimensionar para telas menores
+    }
 });
 
 window.addEventListener('mousemove', (e) => {
@@ -36,3 +50,17 @@ rangeSlider.addEventListener('input', (event) => {
     responsiveCard.style.width = newWidth;
 });
 
+themeToggle.addEventListener('click', () =>{
+    body.classList.toggle('dark-mode')
+
+    if(body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark')
+    }else{
+        localStorage.setItem('theme', 'light')
+    }
+})
+
+const savedTheme = localStorage.getItem('theme')
+if (savedTheme && savedTheme === 'dark'){
+    body.classList.add('dark-mode')
+}
